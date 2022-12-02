c87a4e420d7e8f914f47fa5d579d9b9e667913a5
dano <danielle.dhd1@gmail.com>
2022-12-02 21:41:55 +0200

    create cart, payment and addresses pages

---
+++ app/src/main/java/com/quico/tech/activity/HomeTestActivity.kt
@@ -0,0 +1,11 @@
+package com.quico.tech.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_test)
+    }
+}
---
+++ app/src/main/java/com/quico/tech/model/Item.kt
@@ -0,0 +1,4 @@
+package com.quico.tech.model
+
+class Item {
+}
